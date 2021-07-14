da28af1c6234cc05c1304c750ef262423c139125
Shalini Singh Jaspal <shalinisingh.jaspal@.sheridancollege.ca>
2021-07-14 09:52:49 -0400

    Completed the definition of overriding function Withdraw for ChecquingAccount

--- BankingAppCli/Program.cs
+++ BankingAppCli/Program.cs
@@ -11,8 +11,16 @@ namespace BankingAppCli
             BankAccount account;
             account = new ChecquingAccount(1, "test", 10000);
             account.Deposit(1000);
-            account.Withdraw(1000);
             Console.WriteLine(account.CurrentBalance);
+            Console.WriteLine(account.WithdrawalLimit);
+            account.Withdraw(11000);
+            Console.WriteLine(account.CurrentBalance);
+            Console.WriteLine(account.WithdrawalLimit);
+
+            //todo: Add a class SavingsAccount
+            //todo: Maintain a list of accounts
+            //todo: Populate the list with instances of SA/CA based on the choice of the user
+            //todo: Observe polymorphic behaviour of the BankAccount by performing transactions om these accounts
         }
     }
 }
--- BankingAppLibrary/ChecquingAccount.cs
+++ BankingAppLibrary/ChecquingAccount.cs
@@ -27,7 +27,13 @@ namespace BankingAppLibrary
         /// <param name="amount">Amount (CAD) to be withdrawn</param>
         public override void Withdraw(float amount)
         {
-            throw new NotImplementedException("Working on the logic for withdrawal");
+            if (amount <= 0)
+                throw new ArgumentException("Withdraw amount must be greater than zero");
+            if (amount>WithdrawalLimit)
+                //TODO: throw new NotEnoughBalanceException (----);
+                throw new ArgumentException("Not enough funds to honor this transaction");
+            CurrentBalance -= amount;
         }
+
     }
 }
